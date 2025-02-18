4e42f6486aecc46c3411f47f233406e87b39ff72
poly1997p <poly20221997@gmail.com>
2025-02-18 21:27:05 +0600

    js function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//for sub
 function sub( )
 {
     let number1=30;
@@ -9,7 +9,7 @@ function sub( )
 }
 sub()
 
-
+//for sum
 let rose=40;
 let lily=60;
 let price=rose+lily;
@@ -18,6 +18,7 @@ console.log("these flowers price are="+price);
 
 console.log("hello world");
 
+//for multipication
 function multi(numb1,numb2)
 {
     let digitx=numb1;
@@ -26,3 +27,14 @@ function multi(numb1,numb2)
     console.log("the multipication is="+price);
 }
 multi(30,2);
+
+
+//for divition
+function divi(data1, data2)
+{
+    let digi1=data1;
+    let digi2=data2;
+    let price1=data1/data2;
+    console.log("the divition is="+price1);
+}
+divi(100,2);
