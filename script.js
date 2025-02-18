2a5786bc9b6b51e2f5dc08d9502265c5509a75af
poly1997p <poly20221997@gmail.com>
2025-02-18 21:05:01 +0600

    js function

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function sub( )
+{
+    let number1=30;
+    let number2=10;
+    let result=number1-number2;
+    
+    console.log("the sub is="+result);
+}
+sub()
+
+
+let rose=40;
+let lily=60;
+let price=rose+lily;
+
+console.log("these flowers price are="+price);
+
+console.log("hello world");
+
+function multi(numb1,numb2)
+{
+    let digitx=numb1;
+    let digity=numb2;
+    let price=numb1*numb2;
+    console.log("the multipication is="+price);
+}
+multi(30,2);
